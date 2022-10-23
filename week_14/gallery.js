70a1443199ba19a34552673e42b98a9bedad81b4
RenHayakawa <lizok_k1998@mail.ru>
2022-10-23 21:41:29 +0300

    Неделя 14 - дз: калькулятор, блоки с меняющимся бэкграундом и неработающая галерея

---
+++ week_14/gallery.js
@@ -0,0 +1,11 @@
+function changeImgforward() {
+    let image1 = document.getElementById("img1");
+    image1.src = "./assets/image/sleeping-boy2.jpg";
+    console.log(image1.src);
+}
+
+function changeImgbackward() {
+    let image1 = document.getElementById("img1");
+    image1.src = "./assets/image/sleeping-boy3.jpg"
+    console.log(image1.src);
+}
